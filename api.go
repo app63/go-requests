3c8d274e65a3ee6bc267281604f7c04baf42e026
Aditya Pujari <app63@njit.edu>
2023-02-10 19:36:49 -0600

    base system skeleton

---
+++ api.go
@@ -0,0 +1,43 @@
+package requests
+
+import (
+	"net/http"
+)
+
+func request(method string, url string, data map[string]string) (*http.Response, error) {
+	client := &http.Client{}
+	req, _ := http.NewRequest(method, url, data)
+
+	res, _ := client.Do(req)
+	defer req.Body.Close()
+
+	return res, nil
+}
+
+func Get(url string) (*http.Response, error) {
+	return request("GET", url, nil)
+}
+
+func Options(url string) (*http.Response, error) {
+	return request("OPTIONS", url, nil)
+}
+
+func Head(url string) (*http.Response, error) {
+	return request("HEAD", url, nil)
+}
+
+func Post(url string, data map[string]string) (*http.Response, error) {
+	return request("POST", url, data)
+}
+
+func Put(url string, data map[string]string) (*http.Response, error) {
+	return request("PUT", url, data)
+}
+
+func Patch(url string, data map[string]string) (*http.Response, error) {
+	return request("PATCH", url, data)
+}
+
+func Delete(url string) (*http.Response, error) {
+	return request("DELETE", url, nil)
+}

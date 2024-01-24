82edbac87682927822fac0d8eede1652dcb69818
Aditya Pujari <app63@njit.edu>
2024-01-23 21:26:33 -0600

    Adding logic to set headers & handle errors.

--- api.go
+++ api.go
@@ -4,38 +4,51 @@ import (
 	"net/http"
 )
 
-func request(method string, url string, data map[string]string) (*http.Response, error) {
+func request(method string, url string, headers map[string]string) (*http.Response, error) {
 	client := &http.Client{}
-	req, _ := http.NewRequest(method, url, data)
+	req, err := http.NewRequest(method, url, nil)
 
-	res, _ := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers {
+			req.Header.Set(key, value)
+		}
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
 	defer req.Body.Close()
 
 	return res, nil
 }
 
-func Get(url string) (*http.Response, error) {
+func Get(url string, headers map[string]string) (*http.Response, error) {
 	return request("GET", url, nil)
 }
 
-func Options(url string) (*http.Response, error) {
+func Options(url string, headers map[string]string) (*http.Response, error) {
 	return request("OPTIONS", url, nil)
 }
 
-func Head(url string) (*http.Response, error) {
+func Head(url string, headers map[string]string) (*http.Response, error) {
 	return request("HEAD", url, nil)
 }
 
-func Post(url string, data map[string]string) (*http.Response, error) {
-	return request("POST", url, data)
+func Post(url string, headers map[string]string) (*http.Response, error) {
+	return request("POST", url, headers)
 }
 
-func Put(url string, data map[string]string) (*http.Response, error) {
-	return request("PUT", url, data)
+func Put(url string, headers map[string]string) (*http.Response, error) {
+	return request("PUT", url, headers)
 }
 
-func Patch(url string, data map[string]string) (*http.Response, error) {
-	return request("PATCH", url, data)
+func Patch(url string, headers map[string]string) (*http.Response, error) {
+	return request("PATCH", url, headers)
 }
 
 func Delete(url string) (*http.Response, error) {
